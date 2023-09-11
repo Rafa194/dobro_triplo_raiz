1fea2d9ef35461ee6001474a4b3275f7a4383650
Rafael Prado <rafaelnascimentoprado@hotmail.com>
2023-09-11 20:22:59 -0300

    Primeiro commit

---
+++ 005. Dobro, triplo, raiz quadrada.py
@@ -0,0 +1,7 @@
+valor = int(input("Digite um numero: "))
+dobro = (valor * 2)
+triplo = (valor * 3)
+raiz = (valor ** (1 / 2))
+print('O dobro de {} é {}!'.format(valor, dobro ))
+print('O triplo de {} é {}!'.format(valor, triplo))
+print('A raiz de {} é {}!'.format(valor, raiz))
